367a964f87aaae88808eaaa0cf88d90dc93361ed
Rafeeq Ebrahim <cerafeeq@gmail.com>
2018-08-25 19:02:59 +0530

    Added tabs

---
+++ app/src/main/java/com/rainierlabs/tabbeduidemo/FragmentOne.java
@@ -0,0 +1,31 @@
+package com.rainierlabs.tabbeduidemo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentOne extends Fragment {
+
+
+    public FragmentOne() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragment_one, container, false);
+    }
+
+
+}
